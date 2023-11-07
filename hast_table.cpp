4a5c4ba23f0ab82099974b885fb7a339ada0eae0
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2023-11-07 12:00:32 -0800

    Added the class member function "displayTable"

--- hast_table.cpp
+++ hast_table.cpp
@@ -1,10 +1,10 @@
-#include <iostream>     //need this for COUT and CIN
-using namespace std;    //need this for COUT and CIN
+#include <iostream>  //need this for COUT and CIN
+using namespace std; // need this for COUT and CIN
 #include <iomanip>
 #include <stack>
 #include <vector>
-#include <cstdlib>      // for random numbers
-#include <ctime>        // for random numbers
+#include <cstdlib> // for random numbers
+#include <ctime>   // for random numbers
 
 class DataItem
 {
@@ -21,21 +21,36 @@ public:
 class HashTable
 {
 private:
-    vector<DataItem*> hashArray; //vector holds hash table
+    vector<DataItem *> hashArray; // vector holds hash table
     int arraySize;
-    DataItem* pNonItem;  //for deleted items
+    DataItem *pNonItem; // for deleted items
 
 public:
-    HashTable(int size)  //constructor
+    HashTable(int size) // constructor
     {
         arraySize = size;
-        hashArray.resize(arraySize);  //size the vector
-        for (int j = 0; j < arraySize; j++)  //initialize elements
+        hashArray.resize(arraySize);        // size the vector
+        for (int j = 0; j < arraySize; j++) // initialize elements
         {
             hashArray[j] = NULL;
         }
-        pNonItem = new DataItem(-1);  //deleted item key is -1
+        pNonItem = new DataItem(-1); // deleted item key is -1
     }
 
-    
+    void displayTable()
+    {
+        cout << "Table: ";
+        for (int j = 0; j < arraySize; j++)
+        {
+            if (hashArray[j] != NULL)
+            {
+                cout << hashArray[j]->data << " ";
+            }
+            else
+            {
+                cout << "** ";
+            }
+        }
+        cout << endl;
+    }
 };
