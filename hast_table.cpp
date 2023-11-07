5847a7a58a7329368af5f6211e16bccaecd38630
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2023-11-06 18:36:43 -0800

    Added the libraries that we need for this project

---
+++ hast_table.cpp
@@ -0,0 +1,7 @@
+#include <iostream>     //need this for COUT and CIN
+using namespace std;    //need this for COUT and CIN
+#include <iomanip>
+#include <stack>
+#include <vector>
+#include <cstdlib>      // for random numbers
+#include <ctime>        // for random numbers
